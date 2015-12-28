ecf6421ce223e6d8f5d7894c744f60c18eb6e758
nehal-bista <nehal.erpincloud@gmail.com>
2015-12-28 19:20:38 +0530

    added CalendarContract.Events and CalendarContract

--- src/android/Calendar.java
+++ src/android/Calendar.java
@@ -7,6 +7,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import android.app.Activity;
 import android.content.Intent;
+import 	android.provider.CalendarContract;
+import 	android.provider.CalendarContract.Events;
 
 
 public class Calendar extends CordovaPlugin {
@@ -23,9 +25,12 @@ public class Calendar extends CordovaPlugin {
 					//.putExtra("title", arg_object.getString("title"))
 					//.putExtra("description", arg_object.getString("description"))
 					//.putExtra("eventLocation", arg_object.getString("eventLocation"));
-					.putExtra("title", "title")
-			 		.putExtra("description", "description")
-			 		.putExtra("eventLocation", "eventLocation");
+					
+					.putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true);
+					.putExtra(Events.TITLE, "Neel Birthday");
+					.putExtra(Events.DESCRIPTION, "This is a sample description");
+					.putExtra(Events.EVENT_LOCATION, "My Guest House");
+					.putExtra(Events.RRULE, "FREQ=YEARLY");
 				   this.cordova.getActivity().startActivity(calIntent);
 				   callbackContext.success("Event created.");
 				   return true;
