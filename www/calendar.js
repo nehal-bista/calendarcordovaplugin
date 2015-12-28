22f7e2f24684bdbf85086ef4571230aaeff9a2ac
nehal-bista <nehal.erpincloud@gmail.com>
2015-12-28 18:51:14 +0530

    removed args

--- www/calendar.js
+++ www/calendar.js
@@ -1,17 +1,29 @@
+var exec = require('cordova/exec');
 var calendar = {
-    createEvent: function(title, location, notes, startDate, endDate, successCallback, errorCallback) {
-        cordova.exec(
+    //createEvent: function(title, location, notes, startDate, endDate, successCallback, errorCallback) {
+    createEvent: function(title, location, notes, startDate, endDate) {
+		//alert(title+" - "+ location+" - "+  notes+" - "+  startDate+" - "+  endDate+" - "+  successCallback+" - "+  errorCallback);
+		//alert(exec);
+		function successCallback(message){
+			alert("success: "+message);
+		}
+		function errorCallback(message){
+			alert("failed: "+message);
+		}
+        //cordova.exec(
+        exec(
             successCallback, // success callback function
             errorCallback, // error callback function
             'Calendar', // mapped to our native Java class called "Calendar"
             'addCalendarEntry', // with this action name
-            [{                  // and this array of custom arguments to create our entry
-                "title": title,
-                "description": notes,
-                "eventLocation": location,
-                "startTimeMillis": startDate.getTime(),
-                "endTimeMillis": endDate.getTime()
-            }]
+            //~ [{                  // and this array of custom arguments to create our entry
+                //~ "title": title,
+                //~ "description": notes,
+                //~ "eventLocation": location,
+                //~ "startTimeMillis": startDate.getTime(),
+                //~ "endTimeMillis": endDate.getTime()
+            //~ }],
+            []
         ); 
      }
 }
