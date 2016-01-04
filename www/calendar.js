e10af8a9fd48d7a2690635d1408c4a90e07061e2
nehal <nehal.jadhav@bistasolutions.com>
2016-01-04 18:30:42 +0530

    reset to earlier code

--- www/calendar.js
+++ www/calendar.js
@@ -1,30 +1,18 @@
-var exec = require('cordova/exec');
 var calendar = {
-    //createEvent: function(title, location, notes, startDate, endDate, successCallback, errorCallback) {
-    createEvent: function(title, location, notes, startDate, endDate) {
-		//alert(title+" - "+ location+" - "+  notes+" - "+  startDate+" - "+  endDate+" - "+  successCallback+" - "+  errorCallback);
-		//alert(exec);
-		function successCallback(message){
-			alert("success: "+message);
-		}
-		function errorCallback(message){
-			alert("failed: "+message);
-		}
-        //cordova.exec(
-        exec(
+    createEvent: function(title, location, notes, startDate, endDate, successCallback, errorCallback) {
+        cordova.exec(
             successCallback, // success callback function
             errorCallback, // error callback function
-            'Calendar', // mapped to our native Java class called "Calendar"
+            'Calendar', // mapped to our native Java class called "CalendarPlugin"
             'addCalendarEntry', // with this action name
-             [{                  // and this array of custom arguments to create our entry
-                 "title": title,
-                 "description": notes,
-                 "eventLocation": location,
-                 "startTimeMillis": startDate.getTime(),
-                 "endTimeMillis": endDate.getTime()
-             }],
-            
+            [{                  // and this array of custom arguments to create our entry
+                "title": title,
+                "description": notes,
+                "eventLocation": location,
+                "startTimeMillis": startDate.getTime(),
+                "endTimeMillis": endDate.getTime()
+            }]
         ); 
-     }
+    }
 }
 module.exports = calendar;
